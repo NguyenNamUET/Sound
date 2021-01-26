19201ec82daedde0195b009ead5579cc432c6c33
nguyennamuet <namnguyen12399.coder@gmail.com>
2021-01-26 18:13:01 +0700

    Add files via upload

--- draw.py
+++ draw.py
@@ -24,19 +24,45 @@ class Plot(wx.Panel):
         self.points = [(min(sound_x),0)]
         self.axes.vlines(x=min(sound_x), ymin=min(sound_y), ymax=max(sound_y), colors='red')
         self.axes.vlines(x=max(sound_x), ymin=min(sound_y), ymax=max(sound_y), colors='red')
-        for i in np.arange(0.0, 2.5, 0.5):
-            coor_x = i+random.random()
+        for i in np.arange(0.5, 2.5, 0.5):
+            coor_x = i
             self.points.append((coor_x,0))
             self.axes.vlines(x=coor_x, ymin=min(sound_y), ymax=max(sound_y), colors='orange')
         self.points.append((max(sound_x),0))
         self.canvas = FigureCanvas(self, -1, self.figure)
 
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer.Add(self.canvas, 1, wx.EXPAND)
-        #self.Bind(wx.EVT_SIZE, self.OnSize)
+        # self.sizer = wx.BoxSizer(wx.VERTICAL)
+        # self.sizer.Add(self.canvas, 1, wx.EXPAND)
+        # #self.Bind(wx.EVT_SIZE, self.OnSize)
         self.calculateGrid(None)
-        self.SetSizer(self.sizer)
-        self.Fit()
+        self.linePanel = wx.Panel(self.canvas, size=(2, 1000), pos=(102,427))
+        self.linePanel.SetBackgroundColour("red")
+        self.isDragStarted = False
+        self.linePanel.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
+        self.linePanel.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
+        self.linePanel.Bind(wx.EVT_MOTION, self.OnMouseMove)
+        # self.SetSizer(self.sizer)
+        # self.Fit()
+
+    def OnLeftDown(self, evt):
+        print("ON LEFT MOUSE DOWN")
+        self.isDragStarted = True
+        self.linePanel.CaptureMouse()
+        self.prevMousePos = evt.GetPosition()
+        evt.Skip()
+
+    def OnLeftUp(self, evt):
+        self.isDragStarted = False
+        if self.linePanel.HasCapture():
+            self.linePanel.ReleaseMouse()
+        evt.Skip()
+
+    def OnMouseMove(self, evt):
+        if evt.Dragging() and evt.LeftIsDown() and self.isDragStarted:
+            mousePos = evt.GetPosition()
+            wndPos = self.GetPosition()
+            self.Move(wndPos - (self.prevMousePos - mousePos))
+        evt.Skip()
 
     def calculateGrid(self, event):
         # bbox = self.axes.get_window_extent().transformed(self.figure.dpi_scale_trans.inverted())
@@ -44,7 +70,6 @@ class Plot(wx.Panel):
         # width *= self.figure.dpi
         # height *= self.figure.dpi
         # print(width, height)
-        print(len(self.points))
         x = [x for x,_ in self.points]
         y = [y for _, y in self.points]
         xy_pixels = self.axes.transData.transform(np.vstack([x, y]).T)
@@ -56,139 +81,50 @@ class Plot(wx.Panel):
         ypix = height - ypix
 
         for xp, yp in zip(xpix, ypix):
-            print('{x:0.2f}\t{y:0.2f}'.format(x=xp, y=yp))
             points.append((xp,yp))
 
         print(points)
 
 
-class glCanvas(glcanvas.GLCanvas):
+class WindowDragger(wx.Panel):
     def __init__(self, parent):
-        glcanvas.GLCanvas.__init__(self, parent, -1)
-        self.init = False
-        self.context = glcanvas.GLContext(self)
+        wx.Panel.__init__(self, parent)
+        self.isDragStarted = False
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
+        self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
+        self.Bind(wx.EVT_MOTION, self.OnMouseMove)
 
-        # initial mouse position at first line
-        self.lastx = self.x = points[0][0]
-        self.lasty = self.y = points[0][1]
-        self.size = None
-        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
-        self.Bind(wx.EVT_SIZE, self.OnSize)
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Bind(wx.EVT_LEFT_DOWN, self.OnMouseDown)
-        self.Bind(wx.EVT_LEFT_UP, self.OnMouseUp)
-        self.Bind(wx.EVT_MOTION, self.OnMouseMotion)
-
-    def OnEraseBackground(self, event):
-        pass # Do nothing, to avoid flashing on MSW.
-
-    def OnSize(self, event):
-        wx.CallAfter(self.DoSetViewport)
-        event.Skip()
-
-    def DoSetViewport(self):
-        size = self.size = self.GetClientSize()
-        self.SetCurrent(self.context)
-        glViewport(0, 0, size.width, size.height)
-
-    def OnPaint(self, event):
-        dc = wx.PaintDC(self)
-        self.SetCurrent(self.context)
-        if not self.init:
-            self.InitGL()
-            self.init = True
-        self.OnDraw()
-
-    def OnMouseDown(self, evt):
+    def OnLeftDown(self, evt):
+        self.isDragStarted = True
         self.CaptureMouse()
-        self.x, self.y = self.lastx, self.lasty = evt.GetPosition()
+        self.prevMousePos = evt.GetPosition()
+        evt.Skip()
 
-    def OnMouseUp(self, evt):
-        self.ReleaseMouse()
+    def OnLeftUp(self, evt):
+        self.isDragStarted = False
+        if self.HasCapture():
+            self.ReleaseMouse()
+        evt.Skip()
 
-    def OnMouseMotion(self, evt):
-        if evt.Dragging() and evt.LeftIsDown():
-            self.lastx, self.lasty = self.x, self.y
-            self.x, self.y = evt.GetPosition()
-            self.Refresh(False)
-
-
-class LineCanvas(glCanvas):
-    def InitGL(self):
-        # set viewing projection
-        glClearColor(0,0,0,0)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        #glFrustum(-0.5, 0.5, -0.5, 0.5, 1.0, 3.0)
-
-        # position viewer
-        glOrtho(-200, 200, -200, 200, -1, 1)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-
-        glEnable(GL_DEPTH_TEST)
-        glEnable(GL_LIGHTING)
-        glEnable(GL_LIGHT0)
-
-    def OnDraw(self):
-        # clear color and depth buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        # draw six faces of a cube
-        glBegin(GL_POINTS)
-        # glVertex2f(self.lastx, self.lasty)
-        # glVertex2f(self.lastx, self.lasty+10)
-        for i, j in zip(sound_x, sound_y):
-            glVertex2f(i*50-len(sound_x)/2, j*50)
-        glEnd()
-        glBegin(GL_LINES)
-        glVertex2f(0,100)
-        glVertex2f(0, -100)
-        glVertex2f(100, 0)
-        glVertex2f(-100, 0)
-        glEnd()
-        print(self.x, self.y, self.lastx, self.lasty)
-        # if self.size is None:
-        #     self.size = self.GetClientSize()
-        # w, h = self.size
-        # w = max(w, 1.0)
-        # h = max(h, 1.0)
-        # xScale = 180.0 / w
-        # yScale = 180.0 / h
-        # glRotatef((self.y - self.lasty) * yScale, 1.0, 0.0, 0.0)
-        # # glRotatef((self.x - self.lastx) * xScale, 0.0, 1.0, 0.0)
-
-        self.SwapBuffers()
-
-
-class MainApp(wx.App):
-    def __init__(self):
-        wx.App.__init__(self, redirect=False)
-
-    def OnInit(self):
-        frame = wx.Frame(None, -1, 'CubeCanvas', size=(400, 400))
-        panel = wx.Panel(frame, -1)
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        plot = Plot(panel)
-        self.sizer.Add(plot, 1, wx.LEFT | wx.TOP | wx.GROW)
-        self.sizer.Add(LineCanvas(panel), 1, wx.LEFT | wx.TOP | wx.GROW)
-        panel.SetSizer(self.sizer)
-        panel.Fit()
-
-        frame.Show(True)
-
-        self.frame = frame
-        return True
-
-    def OnExitApp(self, evt):
-        self.frame.Close(True)
-
-    def OnCloseFrame(self, evt):
-        if hasattr(self, "window") and hasattr(self.window, "ShutdownDemo"):
-            self.window.ShutdownDemo()
+    def OnMouseMove(self, evt):
+        if evt.Dragging() and evt.LeftIsDown() and self.isDragStarted:
+            mousePos = evt.GetPosition()
+            wndPos = self.GetPosition()
+            self.Move(wndPos - (self.prevMousePos - mousePos))
         evt.Skip()
 
 
+
+# class DragTest(wx.Frame):
+#     def __init__(self, parent):
+#         wx.Frame.__init__(self, parent)
+#         panel = wx.Panel(self)
+#         self.test =Plot(panel)
+#         self.Show()
+
 if __name__ == '__main__':
-    app = MainApp()
+    app = wx.App(False)
+    frame = wx.Frame(None)
+    panel = Plot(frame)
+    frame.Show()
     app.MainLoop()
--- draw2.py
+++ draw2.py
@@ -1,236 +1,37 @@
-# import wx
-#
-#
-# class MovingLine(wx.Panel):
-#     def __init__(self, parent, slider):
-#         wx.Panel.__init__(self, parent, size=(-1, 30), style=wx.SUNKEN_BORDER)
-#         self.parent = parent
-#         self.slider = slider
-#         self.font = wx.Font(9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
-#             wx.FONTWEIGHT_NORMAL, False, 'Courier 10 Pitch')
-#
-#         self.Bind(wx.EVT_PAINT, self.OnPaint)
-#         self.Bind(wx.EVT_SIZE, self.OnSize)
-#
-#
-#     def OnPaint(self, e):
-#         num = range(75, 700, 75)
-#         dc = wx.PaintDC(self)
-#         dc.SetFont(self.font)
-#         w, h = self.GetSize()
-#
-#         dc.SetPen(wx.Pen('#5C5142'))
-#
-#         dc.DrawLine(self.slider, h, self.slider, h+100)
-#
-#     def OnSize(self, e):
-#         self.Refresh()
-#
-# # class CustomSlider(wx.Slider):
-# #     def getCursorPosition(self):
-# #         self.
-#
-#
-# class Example(wx.Panel):
-#
-#     def __init__(self, *args, **kwargs):
-#         super(Example, self).__init__(*args, **kwargs)
-#
-#         self.InitUI()
-#
-#     def InitUI(self):
-#         self.cw = 75
-#         grid = wx.GridBagSizer(hgap=10, vgap=5)
-#         panel = wx.Panel(self)
-#         sld1panel = wx.Panel(panel)
-#         # sld2panel = wx.Panel(panel)
-#         vbox = wx.BoxSizer(wx.VERTICAL)
-#         hbox = wx.BoxSizer(wx.HORIZONTAL)
-#         hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-#         hbox3 = wx.BoxSizer(wx.HORIZONTAL)
-#         self.sld1 = wx.Slider(sld1panel, value=75, maxValue=750, size=(200, -1),
-#                               style=wx.SL_LABELS)
-#         self.line = MovingLine(panel, self.sld1.GetValue())
-#         self.sld2 = wx.Slider(sld2panel, value=75, maxValue=750, size=(200, -1),
-#                               style=wx.SL_LABELS)
-#         hbox.Add(self.line, 1, wx.EXPAND)
-#         hbox2.Add(sld1panel, 1, wx.EXPAND)
-#         hbox3.Add(self.sld1, 0, wx.LEFT | wx.TOP, 35)
-#
-#         sld1panel.SetSizer(hbox3)
-#         sld1panel.SetSizer(hbox3)
-#
-#         vbox.Add(hbox2, 1, wx.EXPAND)
-#         vbox.Add(hbox, 0, wx.EXPAND)
-#         # self.sld1.SetFocus()
-#         self.Bind(wx.EVT_SCROLL, self.OnScroll1, self.sld1)
-#         self.Bind(wx.EVT_SCROLL, self.OnScroll2, self.sld2)
-#         self.Bind(wx.EVT_PAINT, lambda event: self.OnPaint(event, [0,40,0,150]))
-#         grid.Add(self.sld1, pos=(0, 0), flag=wx.EXPAND)
-#         grid.Add(self.sld2, pos=(5, 0), flag=wx.EXPAND)
-#         bigbox.Add(grid, 0, wx.ALL, 5)
-#         self.SetSizerAndFit(bigbox)
-#         self.Centre()
-#
-#     def OnPaint(self, event, pos):
-#         dc = wx.PaintDC(self)
-#         dc.Clear()
-#         dc.SetPen(wx.Pen(wx.BLACK, 1))
-#         print(self.cw)
-#         dc.DrawLine(self.cw,pos[1],self.cw,pos[3])
-#
-#     def OnScroll1(self, e):
-#         self.cw = self.sld1.GetValue()
-#         self.sld2.SetValue(self.cw)
-#     def OnScroll2(self, e):
-#         self.cw = self.sld2.GetValue()
-#         self.sld1.SetValue(self.cw)
-#
-#
-# def main():
-#     app = wx.App(False)
-#     frame = wx.Frame(None)
-#     panel = Example(frame)
-#     frame.Show()
-#     app.MainLoop()
-#
-#
-# if __name__ == '__main__':
-#     main()
-import wx
-from wx import glcanvas
-from OpenGL.GL import *
-from OpenGL.GLUT import *
-import matplotlib as plt
-plt.use('WXAgg')
-import numpy as np
-import random
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
-from matplotlib.backends.backend_wx import NavigationToolbar2Wx
-from matplotlib.figure import Figure
-import wx.lib.inspection
-x1,y1 = 3, 500
-x2,y2 = 3, -500
-
-
-class glCanvas(glcanvas.GLCanvas):
-    def __init__(self, parent):
-        glcanvas.GLCanvas.__init__(self, parent, -1)
-        self.lastx = self.x = 0
-        self.lasty = self.y = 0
-        self.init = False
-        self.context = glcanvas.GLContext(self)
-
-        # initial mouse position at first line
-        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
-        self.Bind(wx.EVT_SIZE, self.OnSize)
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.Bind(wx.EVT_LEFT_DOWN, self.OnMouseDown)
-        self.Bind(wx.EVT_LEFT_UP, self.OnMouseUp)
-        self.Bind(wx.EVT_MOTION, self.OnMouseMotion)
-
-    def OnEraseBackground(self, event):
-        pass # Do nothing, to avoid flashing on MSW.
-
-    def OnSize(self, event):
-        wx.CallAfter(self.DoSetViewport)
-        event.Skip()
-
-    def DoSetViewport(self):
-        size = self.size = self.GetClientSize()
-        self.SetCurrent(self.context)
-        glViewport(0, 0, size.width, size.height)
-
-    def OnPaint(self, event):
-        dc = wx.PaintDC(self)
-        self.SetCurrent(self.context)
-        if not self.init:
-            self.InitGL()
-            self.init = True
-        self.OnDraw()
-
-    def OnMouseDown(self, evt):
-        self.CaptureMouse()
-        self.x, self.y = self.lastx, self.lasty = evt.GetPosition()
-
-    def OnMouseUp(self, evt):
-        self.ReleaseMouse()
-
-    def OnMouseMotion(self, evt):
-        print(self.x, self.y,  self.lastx, self.lasty)
-        if evt.Dragging() and evt.LeftIsDown():
-            self.lastx, self.lasty = self.x, self.y
-            self.x, self.y = evt.GetPosition()
-            self.Refresh(False)
-
-
-class LineCanvas(glCanvas):
-    def InitGL(self):
-        # set viewing projection
-        glClearColor(0,0,0,0)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        # position viewer
-        glOrtho(-500, 500, -500, 500, -1, 1)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-
-        glEnable(GL_DEPTH_TEST)
-        glEnable(GL_LIGHTING)
-        glEnable(GL_LIGHT0)
-
-    def OnDraw(self):
-        # clear color and depth buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glPushMatrix()
-        glTranslatef((self.x - self.lastx) * 2.75, 0, 0)
-        glBegin(GL_LINES)
-        glVertex2f(x1, y1)
-        glVertex2f(x2, y2)
-        glEnd()
-        glPopMatrix()
-
-        glPushMatrix()
-        glTranslatef((self.x - self.lastx) * 2.75, 0, 0)
-        glBegin(GL_LINES)
-        glVertex2f(x1 + 100, y1)
-        glVertex2f(x2 + 100, y2)
-        glEnd()
-        glLoadIdentity()
-        glPopMatrix()
-
-        self.SwapBuffers()
-
-
-class MainApp(wx.App):
-    def __init__(self):
-        wx.App.__init__(self, redirect=False)
-
-    def OnInit(self):
-        frame = wx.Frame(None, -1, 'CubeCanvas', size=(400, 400))
-        panel = wx.Panel(frame, -1)
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.sizer.Add(LineCanvas(panel), 1, wx.EXPAND)
-
-        panel.SetSizer(self.sizer)
-        panel.Fit()
-
-        frame.Show(True)
-
-        self.frame = frame
-        return True
-
-    def OnExitApp(self, evt):
-        self.frame.Close(True)
-
-    def OnCloseFrame(self, evt):
-        if hasattr(self, "window") and hasattr(self.window, "ShutdownDemo"):
-            self.window.ShutdownDemo()
-        evt.Skip()
-
-
+import matplotlib.pyplot as plt
 if __name__ == '__main__':
-    app = MainApp()
-    wx.lib.inspection.InspectionTool().Show()
-    app.MainLoop()
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    sound_x = np.arange(0.0, 3.0, 0.01)
+    sound_y = np.sin(2 * np.pi * sound_x)
+    collected_points = [(min(sound_x), 0)]
+
+    fig, ax = plt.subplots()
+    points, = ax.plot(sound_x, sound_y)
+    ax.vlines(x=min(sound_x), ymin=min(sound_y), ymax=max(sound_y), colors='red')
+    ax.axes.vlines(x=max(sound_x), ymin=min(sound_y), ymax=max(sound_y), colors='red')
+    for i in np.arange(0.5, 2.5, 0.5):
+        coor_x = i
+        collected_points.append((coor_x, 0))
+        ax.vlines(x=coor_x, ymin=min(sound_y), ymax=max(sound_y), colors='orange')
+    collected_points.append((max(sound_x), 0))
+
+    # Get the x and y data and transform it into pixel coordinates
+    x = [x for x, _ in collected_points]
+    y = [y for _, y in collected_points]
+    xy_pixels = ax.transData.transform(np.vstack([x, y]).T)
+    xpix, ypix = xy_pixels.T
+
+    # In matplotlib, 0,0 is the lower left corner, whereas it's usually the upper
+    # left for most image software, so we'll flip the y-coords...
+    width, height = fig.canvas.get_width_height()
+    ypix = height - ypix
+
+
+    for xp, yp in zip(xpix, ypix):
+        print('{x:0.2f}\t{y:0.2f}'.format(x=xp, y=yp))
+
+    # We have to be sure to save the figure with it's current DPI
+    # (savfig overrides the DPI of the figure, by default)
+    fig.savefig('test.png', dpi=fig.dpi)
